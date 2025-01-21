8785cd27f6abae6009984401880184cfbad04f16
luccabruno3z <luccabruno@sanluis.edu.ar>
2025-01-20 22:55:41 -0300

    Update PRstats_multiclans.py

--- graphs/PRstats_multiclans.py
+++ graphs/PRstats_multiclans.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 import os
-import json  # Asegúrate de importar la librería json
+import json  # Importar la librería json
 from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
 
