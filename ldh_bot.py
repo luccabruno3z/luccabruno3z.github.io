266c91814b704229ad4be2606aec7b11ec03752d
luccabruno3z <luccabruno@sanluis.edu.ar>
2025-05-13 00:14:41 -0300

    Update ldh_bot.py

--- ldh_bot.py
+++ ldh_bot.py
@@ -42,6 +42,7 @@ GITHUB_GRAPH_E_LAM = "https://luccabruno3z.github.io/graphs/E-LAM_interactive_ch
 GITHUB_GRAPH_RIM_LA = "https://luccabruno3z.github.io/graphs/RIM:LA_interactive_chart.html"
 GITHUB_GRAPH_ADG = "https://luccabruno3z.github.io/graphs/ADG_interactive_chart.html"
 GITHUB_GRAPH_FASO = "https://luccabruno3z.github.io/graphs/FASO_interactive_chart.html"
+GITHUB_GRAPH_PORN = "https://luccabruno3z.github.io/graphs/PORN_interactive_chart.html"
 GITHUB_GRAPH_PLAYERS = "https://luccabruno3z.github.io/graphs/all_players_interactive_chart.html"
 
 GITHUB_JSON_LDH = "https://luccabruno3z.github.io/graphs/LDH_players.json"
@@ -57,6 +58,7 @@ GITHUB_JSON_E_LAM = "https://luccabruno3z.github.io/graphs/E-LAM_players.json"
 GITHUB_JSON_RIM_LA = "https://luccabruno3z.github.io/graphs/RIM:LA_players.json"
 GITHUB_JSON_ADG = "https://luccabruno3z.github.io/graphs/ADG_players.json"
 GITHUB_JSON_FASO = "https://luccabruno3z.github.io/graphs/FASO_players.json"
+GITHUB_JSON_PORN = "https://luccabruno3z.github.io/graphs/PORN_players.json"
 GITHUB_JSON_PLAYERS = "https://luccabruno3z.github.io/graphs/all_players_clusters.json"
 
 GITHUB_JSON_CLANS = "https://luccabruno3z.github.io/graphs/clan_averages.json"
